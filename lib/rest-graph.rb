4a941e4f582a787ef3120fb434fbc9ca00609c40
Lin Jen-Shin <godfat@godfat.org>
2010-05-04 00:26:49 +0800

    rest-graph.rb: use struct

--- lib/rest-graph.rb
+++ lib/rest-graph.rb
@@ -3,23 +3,8 @@ require 'rest_client'
 
 require 'cgi'
 
-class RestGraph
-  autoload :Rack, 'rack'
-  def self.parse_token_in_rack_env env, app_id = '\d+'
-    env['HTTP_COOKIE'] =~ /fbs_#{app_id}="(.+?)"/ &&
-      Rack::Utils.parse_query($1)['access_token']
-  end
-
-  def self.parse_token_in_cookies cookies, app_id
-    parse_token_in_fb_cookie(cookies["fbs_#{app_id}"])
-  end
-
-  def self.parse_token_in_fb_cookie fb_cookie
-    fb_cookie && Rack::Utils.parse_query(fb_cookie[1..-2])['access_token']
-  end
-
-  attr_accessor :access_token, :graph_server, :fql_server,
-                :accept, :lang, :auto_decode
+class RestGraph < Struct.new(:access_token, :graph_server, :fql_server,
+                             :accept, :lang, :auto_decode, :app_id, :secret)
   def initialize o = {}
     self.access_token = o[:access_token]
     self.graph_server = o[:graph_server] || 'https://graph.facebook.com/'
