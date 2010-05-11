9009738ff285aef63fd53875ec47afa9173df79a
Lin Jen-Shin <godfat@godfat.org>
2010-05-11 13:01:43 +0800

    rest-graph.rb, test_rest-graph.rb: convert query to_s before CGI.escape

--- test/test_rest-graph.rb
+++ test/test_rest-graph.rb
@@ -160,6 +160,13 @@ describe RestGraph do
     }
   end
 
+  it 'would convert query to string' do
+    mock(o = Object.new).to_s{ 'i am mock' }
+    stub_request(:get, "https://graph.facebook.com/search?q=i%20am%20mock").
+      to_return(:body => 'ok')
+    RestGraph.new(:auto_decode => false).get('search', :q => o).should == 'ok'
+  end
+
   it 'could use module to override default attributes' do
     module BlahAttributes
       def default_app_id
