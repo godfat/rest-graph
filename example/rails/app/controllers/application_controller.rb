71f196f89dcc7eedb1f685e48ce0829b1015e2d8
Lin Jen-Shin <godfat@godfat.org>
2010-05-28 19:40:09 +0800

    add rails example

---
+++ example/rails/app/controllers/application_controller.rb
@@ -0,0 +1,20 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+
+  # Scrub sensitive parameters from your log
+  # filter_parameter_logging :password
+
+  include RestGraph::RailsController
+  before_filter :setup_iframe
+  before_filter :setup_rest_graph
+
+  def index
+    me = @rg.get('me')
+    return unless me
+    render :text => me.to_json
+  end
+end
