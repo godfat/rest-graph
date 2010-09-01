87c76da9d2e654003278ee46905914d7a45f2377
Lin Jen-Shin <godfat@godfat.org>
2010-09-01 16:37:20 +0800

    merge test_access_token.rb into test_misc.rb

--- test/test_misc.rb
+++ test/test_misc.rb
@@ -11,6 +11,19 @@ describe RestGraph do
     RR.verify
   end
 
+  it 'would return true in authorized? if there is an access_token' do
+    RestGraph.new(:access_token => '1').authorized?.should == true
+    RestGraph.new(:access_token => nil).authorized?.should == false
+  end
+
+  it 'would treat oauth_token as access_token as well' do
+    rg = RestGraph.new
+    hate_facebook = 'why the hell two different name?'
+    rg.data['oauth_token'] = hate_facebook
+    rg.authorized?.should == true
+    rg.access_token       == hate_facebook
+  end
+
   it 'would build correct headers' do
     rg = RestGraph.new(:accept => 'text/html',
                        :lang   => 'zh-tw')
