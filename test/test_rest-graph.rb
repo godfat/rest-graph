2808cf806904448f43d2b4c57b40a89ff969b6b6
Lin Jen-Shin <godfat@godfat.org>
2010-09-15 23:04:13 +0800

    fix double require in `rake test`

--- test/test_rest-graph.rb
+++ test/test_rest-graph.rb
@@ -1,10 +1,10 @@
 
+$LOAD_PATH << File.dirname(__FILE__)
+$LOAD_PATH.uniq!
+
 Dir["#{File.dirname(__FILE__)}/*.rb"].each{ |file|
   next if file == __FILE__
-
-  if respond_to?(:require_relative, true)
-    require_relative File.basename(file)
-  else
-    require file
-  end
+  next if ARGV.map{ |path| File.expand_path(path)
+                }.include?(File.expand_path(file))
+  require File.basename(file).sub(/\..+$/, '')
 }
