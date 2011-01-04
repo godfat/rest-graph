290e45528f56fefe0000f4096a3d40fd8f8d2b3c
Lin Jen-Shin <godfat@godfat.org>
2011-01-04 19:19:47 +0800

    test_facebook.rb: add test. sorry, not in a good mood to add more tests

---
+++ test/test_facebook.rb
@@ -0,0 +1,36 @@
+
+if respond_to?(:require_relative, true)
+  require_relative 'common'
+else
+  require File.dirname(__FILE__) + '/common'
+end
+
+require 'rest-graph/facebook_util'
+
+describe RestGraph::FacebookUtil do
+  after do
+    RR.verify
+  end
+
+  before do
+    @res = [{'publish_stream' => 1, 'email' => 0}]
+  end
+
+  should 'fix_permission' do
+    RestGraph.new.fix_permission(@res).should == %w[publish_stream]
+  end
+
+  should 'fix_fql_multi' do
+    RestGraph.new.fix_fql_multi([{'name'=>'a', 'fql_result_set'=> @res}]).
+      should == {'a' => @res}
+  end
+
+  should 'permissions' do
+    mock(rg = RestGraph.new).fql(
+      rg.permissions_fql(1234,
+      RestGraph::FacebookUtil::PERMISSIONS), {}, :secret => true
+    ){ @res }
+
+    rg.permissions(1234).should == %w[publish_stream]
+  end
+end
