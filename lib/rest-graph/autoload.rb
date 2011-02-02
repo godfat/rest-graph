404d334d9847c937451590debe0cd4d109fa7e3a
Lin Jen-Shin <godfat@godfat.org>
2011-02-03 00:36:15 +0800

    autoload.rb: fix message

--- lib/rest-graph/autoload.rb
+++ lib/rest-graph/autoload.rb
@@ -1,4 +1,4 @@
 
 require 'rest-graph'
-puts "[DEPRECATED] require 'rest-graph/auto_load' is deprecated, " \
+puts "[DEPRECATED] require 'rest-graph/autoload' is deprecated, " \
      "now please just use require 'rest-graph'"
