00ff03b04e97a368f9ae91442292c1ddae3c635e
Lin Jen-Shin <godfat@godfat.org>
2011-02-23 17:51:38 +0800

    application_controller_test.rb: local var for url

--- example/rails3/test/functional/application_controller_test.rb
+++ example/rails3/test/functional/application_controller_test.rb
@@ -19,63 +19,67 @@ class ApplicationControllerTest < ActionController::TestCase
     WebMock.reset!
   end
 
-  def test_protected
-    assert_nil @controller.public_methods.find{ |m| m.to_s =~ /^rest_graph/ }
-  end
-
   def test_index
     get(:index)
     assert_response :redirect
-    assert_equal(
-      normalize_url(
-        'https://graph.facebook.com/oauth/authorize?client_id=123&' \
-        'scope=&' \
-        'redirect_uri=http%3A%2F%2Ftest.host%2F'),
-      normalize_url(assigns(:rest_graph_authorize_url)))
+
+    url = normalize_url(
+      'https://graph.facebook.com/oauth/authorize?client_id=123&' \
+      'scope=&redirect_uri=http%3A%2F%2Ftest.host%2F')
+
+    assert_equal(url, normalize_url(assigns(:rest_graph_authorize_url)))
   end
 
   def test_canvas
     get(:canvas)
     assert_response :success
-    assert_equal(
-      normalize_url(
-        'https://graph.facebook.com/oauth/authorize?client_id=123&' \
-        'scope=publish_stream&'                                     \
-        'redirect_uri=http%3A%2F%2Fapps.facebook.com%2Fcan%2Fcanvas'),
-      normalize_url(assigns(:rest_graph_authorize_url)))
+
+    url = normalize_url(
+      'https://graph.facebook.com/oauth/authorize?client_id=123&' \
+      'scope=publish_stream&'                                     \
+      'redirect_uri=http%3A%2F%2Fapps.facebook.com%2Fcan%2Fcanvas')
+
+    assert_equal(url, normalize_url(assigns(:rest_graph_authorize_url)))
   end
 
   def test_diff_canvas
     get(:diff_canvas)
     assert_response :success
-    assert_equal(
-      normalize_url(
-        'https://graph.facebook.com/oauth/authorize?client_id=123&' \
-        'scope=email&'                                              \
-        'redirect_uri=http%3A%2F%2Fapps.facebook.com%2FToT%2Fdiff_canvas'),
-      normalize_url(assigns(:rest_graph_authorize_url)))
+
+    url = normalize_url(
+      'https://graph.facebook.com/oauth/authorize?client_id=123&' \
+      'scope=email&'                                              \
+      'redirect_uri=http%3A%2F%2Fapps.facebook.com%2FToT%2Fdiff_canvas')
+
+    assert_equal(url, normalize_url(assigns(:rest_graph_authorize_url)))
   end
 
   def test_iframe_canvas
     get(:iframe_canvas)
     assert_response :success
-    assert_equal(
-      normalize_url(
-        'https://graph.facebook.com/oauth/authorize?client_id=123&' \
-        'scope=&'                                                   \
-        'redirect_uri=http%3A%2F%2Fapps.facebook.com%2Fzzz%2Fiframe_canvas'),
-      normalize_url(assigns(:rest_graph_authorize_url)))
+
+    url = normalize_url(
+      'https://graph.facebook.com/oauth/authorize?client_id=123&' \
+      'scope=&'                                                   \
+      'redirect_uri=http%3A%2F%2Fapps.facebook.com%2Fzzz%2Fiframe_canvas')
+
+    assert_equal(url, normalize_url(assigns(:rest_graph_authorize_url)))
   end
 
   def test_options
     get(:options)
     assert_response :redirect
-    assert_equal(
-      normalize_url(
-        'https://graph.facebook.com/oauth/authorize?client_id=123&' \
-        'scope=bogus&'                                              \
-        'redirect_uri=http%3A%2F%2Ftest.host%2Foptions'),
-      normalize_url(assigns(:rest_graph_authorize_url)))
+
+    url = normalize_url(
+      'https://graph.facebook.com/oauth/authorize?client_id=123&' \
+      'scope=bogus&'                                              \
+      'redirect_uri=http%3A%2F%2Ftest.host%2Foptions')
+
+    assert_equal(url, normalize_url(assigns(:rest_graph_authorize_url)))
+  end
+
+  def test_protected
+    assert_nil @controller.public_methods.find{ |m| m.to_s =~ /^rest_graph/ }
   end
 
   def test_no_auto
