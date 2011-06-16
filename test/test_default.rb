50660256090999b72352a0956fff633d357289b4
Lin Jen-Shin <godfat@godfat.org>
2011-06-16 18:32:18 +0800

    test_default.rb: better default tests copied from rest-core

--- test/test_default.rb
+++ test/test_default.rb
@@ -7,25 +7,21 @@ end
 
 describe RestGraph do
   should 'honor default attributes' do
-    TestHelper.attrs_no_callback.each{ |name|
-      RestGraph.new.send(name).should ==
-        RestGraph.send("default_#{name}")
-
-      RestGraph.new.send(name).should ==
-        RestGraph::DefaultAttributes.send("default_#{name}")
+    RestGraph.members.reject{ |name|
+      name.to_s =~ /method$|handler$|detector$/ }.each{ |name|
+        RestGraph.new.send(name).should ==
+        RestGraph    .send("default_#{name}")
     }
   end
 
   should 'use module to override default attributes' do
-    module BlahAttributes
+    klass = RestGraph.dup
+    klass.send(:extend, Module.new do
       def default_app_id
         '1829'
       end
-    end
+    end)
 
-    TestHelper.ensure_rollback{
-      RestGraph.send(:extend, BlahAttributes)
-      RestGraph.default_app_id.should == '1829'
-    }
+    klass.new.app_id.should == '1829'
   end
 end
